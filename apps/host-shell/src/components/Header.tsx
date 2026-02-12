dd304c658b5e8e61a7fad4fbeff0cd93cc4ff0e2
tienplt <phanletrongtien@gmail.com>
2026-02-12 20:37:41 +0700

    ci

--- apps/host-shell/src/components/Header.tsx
+++ apps/host-shell/src/components/Header.tsx
@@ -2,8 +2,8 @@ import { ThemeToggle } from './ThemeToggle';
 
 export function Header() {
   return (
-    <nav className="p-4 bg-blue-600 text-white flex items-center justify-between">
-      <h1 className="m-0 font-semibold text-2xl text-red-500">LakeTest</h1>
+    <nav className="px-1.5 py-2 bg-[--bg-primary] text-[--color-primary] border-b boder-b-[--color-primary] flex items-center justify-between">
+      <h1 className="m-0 font-semibold text-base">Networks</h1>
       <ThemeToggle />
     </nav>
   );
--- apps/host-shell/src/components/ThemeToggle.tsx
+++ apps/host-shell/src/components/ThemeToggle.tsx
@@ -6,7 +6,7 @@ export function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
+      className="px-4 py-2 rounded-md bg-none transition-colors"
       aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
     >
       {theme === 'light' ? '🌙' : '☀️'}
